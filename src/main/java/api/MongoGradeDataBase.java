acbd39ee125ef4558275e0112a1de4aeda1a7525
mzhang055 <mzhang055@gmail.com>
2025-10-06 17:49:52 -0400

    task 3a and 3b

--- src/main/java/api/MongoGradeDataBase.java
+++ src/main/java/api/MongoGradeDataBase.java
@@ -247,10 +247,6 @@ public class MongoGradeDataBase implements GradeDataBase {
     }
 
     @Override
-    // TODO Task 3b: Implement this method
-    //       Hint: Read the Grade API documentation for getMyTeam (link below) and refer to the above similar
-    //             methods to help you write this code (copy-and-paste + edit as needed).
-    //             https://www.postman.com/cloudy-astronaut-813156/csc207-grade-apis-demo/folder/isr2ymn/get-my-team
     public Team getMyTeam() {
         final OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
@@ -264,10 +260,31 @@ public class MongoGradeDataBase implements GradeDataBase {
         final Response response;
         final JSONObject responseBody;
 
-        // TODO Task 3b: Implement the logic to get the team information
-        // HINT 1: Look at the formTeam method to get an idea on how to parse the response
-        // HINT 2: You may find it useful to just initially print the contents of the JSON
-        //         then work on the details of how to parse it.
-        return null;
+        try {
+            response = client.newCall(request).execute();
+            responseBody = new JSONObject(response.body().string());
+
+            if (response.code() == 200) {
+                // Extract the "team" object from the response
+                final JSONObject teamData = responseBody.getJSONObject("team");
+                final String teamName = teamData.getString("name");
+
+                // Extract members array
+                final JSONArray membersArray = teamData.getJSONArray("members");
+                final String[] members = new String[membersArray.length()];
+                for (int i = 0; i < membersArray.length(); i++) {
+                    members[i] = membersArray.getString(i);
+                }
+
+                // Return new Team object
+                return new Team(teamName, members);
+
+            } else {
+                throw new RuntimeException(responseBody.getString("message"));
+            }
+
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -23,11 +23,24 @@ public final class GetAverageGradeUseCase {
         // Call the API to get usernames of all your team members
         float sum = 0;
         int count = 0;
-        // TODO Task 3b: Go to the MongoGradeDataBase class and implement getMyTeam.
         final Team team = gradeDataBase.getMyTeam();
-        // Call the API to get all the grades for the course for all your team members
-        // TODO Task 3a: Complete the logic of calculating the average course grade for
-        //              your team members. Hint: the getGrades method might be useful.
+
+        // Get all team members
+        final String[] members = team.getMembers();
+
+        // For each team member, get their grades for the course
+        for (String member : members) {
+            final Grade[] grades = gradeDataBase.getGrades(member);
+
+            // Find the grade for the specified course
+            for (Grade grade : grades) {
+                if (grade.getCourse().equals(course)) {
+                    sum += grade.getGrade();
+                    count++;
+                    break; // Found the course, move to next member
+                }
+            }
+        }
 
         if (count == 0) {
             return 0;
